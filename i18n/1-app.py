17b10cfb2138e0e852a9ce365f0d2bff05784f74
aysunhp <bd7zatzha@code.edu.az>
2026-04-08 11:57:00 +0000

    task 1 added

---
+++ i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Flask  Babel application"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """Configuration class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEAFULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello_world():
+    """Route that returns a template"""
+    return render_template('0-index.html')
