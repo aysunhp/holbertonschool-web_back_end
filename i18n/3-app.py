7e0499b57da3b3c0a735db510334c8407c9c6ba2
aysunhp <bd7zatzha@code.edu.az>
2026-04-17 17:50:17 +0400

    task 3 added

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python3
-""" Flask  Babel application"""
+"""
+Basic Flask app with Babel internationalization
+"""
 
-from flask import Flask, render_template
-from flask_babel import Babel, _
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
 app = Flask(__name__)
 
 
-class Config(object):
-    """Configuration class for Flask app"""
+class Config:
+    """
+    Babel config
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
+
 babel = Babel(app)
 
 
 def get_locale():
-    """Select a language translation to use for the request"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """
+    Get locale from URL parameter
+    """
+    return request.args.get("locale", "en")
 
 
 babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/")
-def hello_world():
-    """Route that returns a template"""
-    return render_template('3-index.html')
+def home():
+    """
+    Home page
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
